ddec3d40af563598de90df2decf5b8dc8f59b009
fredsterss <fredsters_s@me.com>
2013-10-31 16:35:39 -0700

    blog templateeee

--- config.rb
+++ config.rb
@@ -1,6 +1,41 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page "/feed.xml", :layout => false
+
+activate :livereload
+
+### 
 # Compass
 ###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -12,21 +47,22 @@
 ###
 
 # Per-page layout changes:
-#
+# 
 # With no layout
 # page "/path/to/file.html", :layout => false
-#
+# 
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-#
+# 
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
 
 ###
 # Helpers
@@ -34,9 +70,6 @@
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -55,16 +88,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-
+  
   # Minify Javascript on build
   # activate :minify_javascript
-
+  
   # Enable cache buster
-  # activate :asset_hash
-
+  # activate :cache_buster
+  
   # Use relative URLs
   # activate :relative_assets
-
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_path, "/Content/images/"
 end
